fe73dd7638db334280a99af074059da555558e5e
osahtout <osahtout@hotmail.com>
2020-10-14 17:48:03 -0400

    I think dataset 1 is finished

--- dataset1.py
+++ dataset1.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
 import sklearn.naive_bayes
-import sklearn.metrics         # For confusion matrix, precision, recall, and f1-measure
-import sklearn.svm             # For SVC class
 import sklearn.preprocessing   # For scale function
 import sklearn.metrics         # for accuracy_score
 import sys
@@ -44,12 +42,7 @@ test_with_label_2 = 'dataset/test_with_label_2.csv'
 train_1_csv = np.loadtxt(train_1, delimiter=',',  skiprows=0,) 
 val_1_csv = np.loadtxt(value_1, delimiter=',',  skiprows=0)
 letter_1 = np.loadtxt(info_1, delimiter=',',  skiprows=1, usecols=1, dtype=np.str)
-
-# test_no_label_1_csv = np.loadtxt(test_no_label_1, delimiter=',',  skiprows=0)
 test_with_label_1_csv = np.loadtxt(test_with_label_1, delimiter=',',  skiprows=0, dtype='int32')
-
-print(test_with_label_1_csv)
-
 
 
 # split the matrices
@@ -61,19 +54,12 @@ train_1_X = train_1_csv[:, :-1 ]
 GNB = sklearn.naive_bayes.GaussianNB().fit(train_1_X, train_1_Y) # naive bayes GaussianNB
 
 
-
-val_1_X = val_1_csv[:, :-1]
-val_1_y = val_1_csv[:, -1]
+# split the array
+val_1_X = val_1_csv[:, :-1] # gets everything except last column
+val_1_y = val_1_csv[:, -1] # gets only last column
 
 
 val_1_y_predict = GNB.predict(val_1_X) # train the model
-
-confusion_matrix_1 = sklearn.metrics.confusion_matrix(val_1_y, val_1_y_predict) # confusion matrix
-report = sklearn.metrics.classification_report(val_1_y, val_1_y_predict, target_names=letter_1) # all the other stuff required
-
-
-print(report)
-# print(confusion_matrix_1)
 
 
 test_with_label_1_X = test_with_label_1_csv[:, :-1 ]
@@ -82,8 +68,17 @@ test_with_label_1_Y = test_with_label_1_csv[:, -1]
 test_with_label_1_Y_predict = GNB.predict(test_with_label_1_X)
 
 confusion_matrix_1_test = sklearn.metrics.confusion_matrix(val_1_y, val_1_y_predict) # confusion matrix
-report_1_test = sklearn.metrics.classification_report(test_with_label_1_X, test_with_label_1_Y_predict, target_names=letter_1) # all the other stuff required
+report_1_test = sklearn.metrics.classification_report(test_with_label_1_Y, test_with_label_1_Y_predict, target_names=letter_1) # all the other stuff required
 
+index = np.arange(1, test_with_label_1_Y_predict.size + 1, 1) 
+array_to_write_to_file = np.stack((index, test_with_label_1_Y_predict), axis=1)
+
+print("\nall\n")
+print(array_to_write_to_file)
+print("\n")
+print("matrix\n")
+print(confusion_matrix_1_test)
+print("\nreport\n")
 print(report_1_test)
 
 
